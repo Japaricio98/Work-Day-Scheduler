12c7509e3ead4adf0b9a1080c7d78abb75f986fa
Juan Aparicio <juan_aparicio98@icloud.com>
2022-10-16 15:48:00 -0500

    added current time

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,4 +1,8 @@
+//Get current day after the page has loaded
 window.addEventListener("DOMContentLoaded", (event) => {
     var todaysDate = moment().format("dddd, MMMM Do").toString();
     document.querySelector("#currentDay").innerHTML = todaysDate;
 });
+
+var currentTime = moment().hour();
+console.log(currentTime);
