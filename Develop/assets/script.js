219f7fc1a92f4b0af731069a903a6bd5889a2da2
Juan Aparicio <juan_aparicio98@icloud.com>
2022-10-16 16:18:54 -0500

    compared current time to the time block on schedule, and changed the time block colors

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -4,5 +4,21 @@ window.addEventListener("DOMContentLoaded", (event) => {
     document.querySelector("#currentDay").innerHTML = todaysDate;
 });
 
+// Get current hour the user loaded the page
 var currentTime = moment().hour();
-console.log(currentTime);
+//console.log(currentTime);
+
+// Change the color of time blocks
+$(".time-block").each(function () {
+    var scheduleTime = $(this).attr("id");
+    //console.log(scheduleTime);
+
+    if (scheduleTime < currentTime) {
+        $(this).children("textarea").addClass("past");
+    } else if (scheduleTime == currentTime) {
+        $(this).children("textarea").addClass("present");
+    } else {
+        $(this).children("textarea").addClass("future");
+    }
+});
+
